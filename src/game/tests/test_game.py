81193670eff831be3c8692d41e558962a35964ec
zyhou <richard.maxime9@gmail.com>
2017-01-31 11:06:31 +0100

    add new line at the end

--- src/game/tests/test_game.py
+++ src/game/tests/test_game.py
@@ -8,9 +8,3 @@ class TestGame(unittest.TestCase):
         self.assertEqual(switchPlayer(1), 2)
         self.assertEqual(switchPlayer(2), 1)
 
-    #def test_askPosition(self):
-        #board = newBoard(12)
-
-        #self.assertEqual(askPosition(board, 1))
-        #self.assertEqual(askPosition(board, 2))
-
