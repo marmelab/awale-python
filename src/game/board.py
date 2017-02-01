e2b558389bd23c5862455da227c79408d8f3e301
zyhou <richard.maxime9@gmail.com>
2017-02-01 15:42:31 +0100

    more explicite condition

--- src/game/board.py
+++ src/game/board.py
@@ -14,9 +14,12 @@ def create_board(size):
 
 
 def can_player_apply_position(player, board, position):
-    move_possible = (player['min_position'] <= position
-                     < player['max_position'] and
-                     board[position] != 0)
+    is_empty_pit = (board[position] == 0)
+    is_player_can_move = (player['min_position'] <=
+                          position <
+                          player['max_position'])
+
+    move_possible = is_player_can_move and not is_empty_pit
 
     if sum(board[player['min_pick']:player['max_pick']]) == 0:
         return move_possible  # todo add condition on starve and feed
