f242f6f5d14486bf83049b218fadcbbf97100f3a
zyhou <richard.maxime9@gmail.com>
2017-02-01 09:56:58 +0100

    split player ui from game

--- src/game/board.py
+++ src/game/board.py
@@ -10,10 +10,5 @@ def create_board(size):
 
 
 def can_player_apply_position(player, board, position):
-
-    if sum(board[player['min_pick']:player['max_pick']]) == 0:
-        print('oui')
-    else:
-        print('non')
         return (player['min_position'] <= position < player['max_position'] and
                 board[position] != 0)
--- src/game/game.py
+++ src/game/game.py
@@ -1,6 +1,7 @@
 from .board import create_board, can_player_apply_position
 from .renderer import render
 
+
 def create_player(number):
         return {
             'number': number,
@@ -10,7 +11,8 @@ def create_player(number):
             'max_pick': (2 - number) * 6,
         }
 
-def start():
+
+def start(type_player):
     print("\n######### GAME STARTED ############\n")
 
     board = create_board(12)  # Constants ?
@@ -20,7 +22,7 @@ def start():
 
     current_player = player
 
-    position = ask_position(board, current_player)
+    position = type_player.ask_position(board, current_player)
     if position < 0:
         print("Invalid position")
 
@@ -28,28 +30,10 @@ def start():
     switch_player(current_player)
 
 
-def ask_position(board, current_player):
-
-    try:
-        position = int(input("Player ({0}), which position: "
-                             .format(current_player['number'])))
-    except ValueError:
-        position = -1
-
-    while not can_player_apply_position(current_player, board, position):
-        try:
-            position = int(input("Wrong move, play again which position: "))
-        except ValueError:
-            position = -1
-
-    return position
-
-
 def play_turn(board, current_player, position):
     print("Player ({}) play {}.".format(current_player['number'], position))
     end_position = deal_position(board, position)
     print(render(board))
-    print(end_position)
 
 
 def deal_position(board, position):
